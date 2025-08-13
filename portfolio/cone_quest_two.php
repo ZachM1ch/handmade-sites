a8cf8c63936d99e4be2b3b3ae14e9f0a2740e17c
Zachary Michniewicz <ztmich@gmail.com>
2025-08-13 17:56:43 -0400

    Update cone_quest_two.php

--- portfolio/cone_quest_two.php
+++ portfolio/cone_quest_two.php
@@ -1,7 +1,7 @@
 <?php $pageTitle = "The Cone Quest 2"; include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
 
 <h1>The Cone Quest 2</h1>
-<h2>The Intrepid Journey to Chillville</h2>
+<h3>The Intrepid Journey to Chillville</h3>
 <p>TEMP WORDS<br>
 <br>Languages and Tools Used: Unity, GitHub Desktop, C#</p>
 <!--<a href="/php/download.php?file=project1.zip">Download Project</a>-->
