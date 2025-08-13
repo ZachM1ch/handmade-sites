94dd307fe0e1dc9ac78d34c20c66a3d9d3d2e664
Zachary Michniewicz <ztmich@gmail.com>
2025-08-13 17:56:01 -0400

    Update index.php

--- portfolio/index.php
+++ portfolio/index.php
@@ -4,11 +4,11 @@
 <p>Here are some of my coding projects:</p>
 
 <ul>
-    <li><a href="bread_boy.php">Project One</a></li>
-	<li><a href="cooler_birds.php">Project One</a></li>
-	<li><a href="deck_tool.php">Project One</a></li>
-	<li><a href="cone_quest_one.php">Project One</a></li>
-	<li><a href="cone_quest_two.php">Project One</a></li>
+    <li><a href="bread_boy.php">Bread Boy</a></li>
+	<li><a href="cooler_birds.php">CoolerBirds.com</a></li>
+	<li><a href="deck_tool.php">MTG DeckTool</a></li>
+	<li><a href="cone_quest_one.php">The Cone Quest</a></li>
+	<li><a href="cone_quest_two.php">The Cone Quest 2</a></li>
 </ul>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
