c51f5b36e7fe1967de0bcf84b2ccb24523c16d0c
Zachary Michniewicz <ztmich@gmail.com>
2025-08-15 17:27:17 -0400

    Added more stories and edited themes

---
+++ stories/Suncrest-County/04.php
@@ -0,0 +1,11 @@
+<?php $pageTitle = "SCC-04"; include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
+
+<h3>Suncrest County Chronicles #4</h3>
+<h2>This Story Has Been Lost To Time.</h2>
+
+<nav>
+    <a href="02.php">← Previous Story</a>
+	<a href="04.php">Next Story →</a>
+</nav>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
--- stories/Suncrest-County/index.php
+++ stories/Suncrest-County/index.php
@@ -5,6 +5,12 @@
     <li><a href="01.php">The New Professor in Suncrest County</a></li>
     <li><a href="02.php">A Written Account on the Events of 12/21 as Witnessed by the Sole Survivor Clara Bishop</a></li>
 	<li><a href="03.php">An Overdue Reunion</a></li>
+	<li><a href="04.php">###############</a></li>
+	<li><a href="05.php">Every Third Sunday in April</a></li>
+	<li><a href="06.php">Pablo's Picnic</a></li>
+	<li><a href="07.php">She Would Always Call Him "T"</a></li>
+	<li><a href="08.php">It's 10 PM</a></li>
+	<li><a href="09.php">Recovered Item 9C</a></li>
 </ul>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
