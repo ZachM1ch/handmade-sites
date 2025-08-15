754434edaa7006aba2f1a7207dd42bb2cc57b4ec
Zachary Michniewicz <ztmich@gmail.com>
2025-08-15 17:48:33 -0400

    added 10-12

---
+++ stories/Suncrest-County/10.php
@@ -0,0 +1,16 @@
+<?php $pageTitle = "SCC-10"; include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
+
+<h3>Suncrest County Chronicles #10</h3>
+<h1>[REDACTED]</h1>
+<div class=card>
+<pre>To Be Discovered Soon.
+
+</pre>
+</div>
+
+<nav>
+    <!--<a href="09.php">← Previous Story</a>-->
+	<!--<a href="11.php">Next Story →</a>-->
+</nav>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
---
+++ stories/Suncrest-County/11.php
@@ -0,0 +1,16 @@
+<?php $pageTitle = "SCC-11"; include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
+
+<h3>Suncrest County Chronicles #11</h3>
+<h1>[REDACTED]</h1>
+<div class=card>
+<pre>To Be Discovered Soon.
+
+</pre>
+</div>
+
+<nav>
+    <!--<a href="10.php">← Previous Story</a>-->
+	<!--<a href="12.php">Next Story →</a>-->
+</nav>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
