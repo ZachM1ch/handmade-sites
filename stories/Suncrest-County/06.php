c1ef865114cfb91ccd9c1b640ac140e364d0f236
Zachary Michniewicz <ztmich@gmail.com>
2025-08-15 17:41:10 -0400

    Updating themes and TBA stories

--- stories/Suncrest-County/06.php
+++ stories/Suncrest-County/06.php
@@ -1,6 +1,6 @@
 <?php $pageTitle = "SCC-03"; include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); ?>
 
-<h3>Suncrest County Chronicles #3</h3>
+<h3>Suncrest County Chronicles #6</h3>
 <h1>Pablo's Picnic</h1>
 <div class=card>
 <pre><i style="font-family: Courier New;">The Sun is not closer.  There, now I’ve said it.</i>
