1e0b034d523513ea7e7fe105faba97ad99113ef8
Zachary Michniewicz <ztmich@gmail.com>
2025-08-18 13:52:42 -0400

    Update 05.php

--- stories/Suncrest-County/05.php
+++ stories/Suncrest-County/05.php
@@ -22,14 +22,37 @@
 
 
 
-	Do not go outside today.
-	Thank you.
+
+
+
+
+
+
+
+
+	<b style="font-family: Brush Script MT; font-size: 20px;">Do not go outside today.
+	Thank you.</b>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 	Quite frankly I was baffled.  Initially I believed this to be some sort of prank, but I didn’t understand what on earth it could mean.  After concluding my interaction with the strange mail piece, my gaze drifted to the window across the kitchen.
 
-	The sky was a deep gray, monotone in contrast, but ever so slightly swirling.  The clouds were roiled up something vaguely fierce.  One wouldn’t be able to distinguish between each cloud if it weren’t for the general sensation of motion they evoked.  They did not appear to be moving when viewed, they simply were.  And I knew it.  Belaying that sun cover was a torrent of rain, each drop slamming hard upon the unwilling surface below.  It was near impossible to differentiate between the drops from the speed at which they descended.  It was not entirely impossible, however.  Every drop that hit a surface created a small pop of liquid that forged the appearance of a shield to protect the impending recipients of rain.  Thought that was only the appearance of a shield.  The commotion in the air brought with it a thick fog, caressing everything beyond a few dozen feet in a blanket of pale heather.  All things combined, it looked to be quite the dreary day.  Almost in spite of the letter I had thought to myself, This is a poor morning to go outside anyway.
+	The sky was a deep gray, monotone in contrast, but ever so slightly swirling.  The clouds were roiled up something vaguely fierce.  One wouldn’t be able to distinguish between each cloud if it weren’t for the general sensation of motion they evoked.  They did not appear to be moving when viewed, they simply were.  And I knew it.  Belaying that sun cover was a torrent of rain, each drop slamming hard upon the unwilling surface below.  It was near impossible to differentiate between the drops from the speed at which they descended.  It was not entirely impossible, however.  Every drop that hit a surface created a small pop of liquid that forged the appearance of a shield to protect the impending recipients of rain.  Thought that was only the appearance of a shield.  The commotion in the air brought with it a thick fog, caressing everything beyond a few dozen feet in a blanket of pale heather.  All things combined, it looked to be quite the dreary day.  Almost in spite of the letter I had thought to myself, <i>This is a poor morning to go outside anyway.</i>
 
 	The season so far had been cool, and knowing I wasn’t to leave my home, I lit the fireplace and cozied in nicely upon a plush chair with my coffee and a book I had not yet completed.  I used the ominous envelope and its contents as kindling to kickstart my accompanying little flame.
 
@@ -37,7 +60,7 @@
 
 	The weather today was gorgeous.  Rays of  delicate warming sunlight broke through the puffy clouds that had become traipsed across the baby blue sky.  The temperature was ever so warmer than usual as it had been getting as the years flew by.  The foliage was bright and abundant, flowers blooming around each outcropping of grass.  A nice breeze tickled the window sill in the kitchen.  I would not open that window or any others.  I was to follow the instructions and remain indoors for the duration of the day.  There was no need to venture out, it was a Sunday after all.  Work would resume the following day without any issue.  But today was a day to stay inside.  
 
-	Eight years ago I started marking the third Sunday of April on my calendar as a reminder to NEVER make plans for this day.  I had always lied since then to any friends, family, or coworkers that asked to spend time together.  A doctor’s appointment.  A dentist appointment.  A surgery.  A veterinarian visit.  I own no pets.  A dog would need to be let outside.  364 days of the year it would be fine, but for this day… it would not be right.  No pets.  For the past few years I always did a little extra grocery shopping to ensure I would have enough food and essentials to make it through today.  I began purchasing books and puzzles to occupy my time and make the day go by faster.  For as much as I dread it, the third Sunday of April sort of became a miniature holiday for me and me alone.  A day to look forward to and a day to never look back upon if I could help it.  But sometimes I cannot help it.
+	Eight years ago I started marking the third Sunday of April on my calendar as a reminder to <b>NEVER</b> make plans for this day.  I had always lied since then to any friends, family, or coworkers that asked to spend time together.  A doctor’s appointment.  A dentist appointment.  A surgery.  A veterinarian visit.  I own no pets.  A dog would need to be let outside.  364 days of the year it would be fine, but for this day… it would not be right.  No pets.  For the past few years I always did a little extra grocery shopping to ensure I would have enough food and essentials to make it through today.  I began purchasing books and puzzles to occupy my time and make the day go by faster.  For as much as I dread it, the third Sunday of April sort of became a miniature holiday for me and me alone.  A day to look forward to and a day to never look back upon if I could help it.  But sometimes I cannot help it.
 
 	Thirteen years ago was the first and only time I defied the letter.  Every year before that had either had poor weather or I had no reason to leave my home.  Thirteen years ago was a day much like today.  Lovely weather that was just begging to be enjoyed.  As had quickly become my ritual, I opened the annual letter over some breakfast coffee.  It had the left half of a stamp on it.  I read it aloud mockingly then tossed the instructional thing to the side of my dining table.  After dressing myself to enjoy the day, I took a final look out the kitchen window at the pleasant view.  Calmly, I strode toward the front door and swung it open, eager to be confronted by the warmth of the sun and a gentle breeze.
 
@@ -45,7 +68,40 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	Beyond my door was… there was no beyond.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	I stood just before the doorway staring out at the purest, emptiest, hollowest nothing I had ever attempted to comprehend.  I had felt as though reality itself was playing a trick on me.  Closing and reopening the door changed nothing but the state of how ajar the block of wood had been.  I sprinted back to the kitchen, leaving the door wide open.  The window still displayed the majestic day I was promised that morning.  I again turned toward the front door.  Empty.  
@@ -69,6 +125,7 @@
 
 	I haphazardly unlatched the window and thrust it open.  That was my greatest mistake yet.  
 	Null.
+
 
 
 
