d54e26da3a84e2c46f14f9a2aba75e8877a18009
Zachary Michniewicz <ztmich@gmail.com>
2025-08-18 14:45:18 -0400

    Updated cone quest pages

--- portfolio/cone_quest_one.php
+++ portfolio/cone_quest_one.php
@@ -4,11 +4,11 @@
 <h2>A Sprinkled Climb to the Stars</h2>
 <p>The Cone Quest is a third person platformer video game created for TheXPlace’s first Summer Game Jam.  Myself and 5 other <br>
 friends and colleagues teamed up to make a game around the theme of melting within a week.  Given the short timeframe and <br>
-the nature of game jams, The Cone Quest is a fairly short game about a little man made out of ice cream jumping across food as <br>
-he climbs to the top of a refrigerator while slowly melting the whole way up.  All of the assets involved were created from scratch by <br>
-the artists and sound engineer on my team.  My role was one of two programmers as well as the level designer and partial team <br>
-lead.  Not all planned features were able to be included in the narrow time limit, but we are currently continuing to work on The <br>
-Cone Quest to bring it closer to our original vision.<br>
+the nature of game jams, The Cone Quest is a fairly short game about a little man made out of ice cream named Terrance <br>
+jumping across food as he climbs to the top of a refrigerator while slowly melting the whole way up.  All of the assets <br>
+involved were created from scratch by the artists and sound engineer on my team.  My role was one of two programmers as well as <br>
+the level designer and partial team lead.  Not all planned features were able to be included in the narrow time limit, but <br>
+small adjustments were made after the jam ended to bring the game closer to our initial goal.<br>
 <br>Languages and Tools Used: Unity, GitHub Desktop, C#</p>
 
 <a href="https://team-cone-quest.itch.io/the-cone-quest">
--- portfolio/cone_quest_two.php
+++ portfolio/cone_quest_two.php
@@ -2,7 +2,13 @@
 
 <h1>The Cone Quest 2</h1>
 <h3>The Intrepid Journey to Chillville</h3>
-<p>TEMP TEXT<br>
+<p>The Cone Quest 2 is a third person platformer video game created for TheXPlace’s second Summer Game Jam (2024). <br>
+Myself and 6 other friends and colleagues teamed up to make a game around the theme of travel within a week.  Terrance returns <br>
+as the protagonist of this sequel, running and jumping through a house trying to make his way outside and into a coller packed <br>
+into a car before the homeowners leave without him.  All of the assets involved were created from scratch by the artists and <br>
+sound engineer on my team and were either brand new for this jam or reused from the previous Cone Quest game. My role was one of <br>
+three programmers as well as the level designer team lead.  Not all planned assets and features were able to be included in the <br>
+narrow time limit, but we have released a small updated version after the jam to bring it closer to our original vision.<br>
 <br>Languages and Tools Used: Unity, GitHub Desktop, C#</p>
 
 <a href="https://team-cone-quest.itch.io/the-cone-quest-2">
