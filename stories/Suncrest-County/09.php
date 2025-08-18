fc2031ec4998460d01d05d9e1231af77edacc1c3
Zachary Michniewicz <ztmich@gmail.com>
2025-08-18 14:24:55 -0400

    Editing SCC09

--- stories/Suncrest-County/09.php
+++ stories/Suncrest-County/09.php
@@ -5,7 +5,9 @@
 <div class=card>
 
 <pre>	Recovered Item 9S: A 5 page document titled “A Comprehensive Timeline of Particularly Poignant Points in New Jersey History” written by Kathy S. Carter [likely a pseudonym given surviving county records].
+
 	This document was recovered from the Obston Creek Library flooding caused by Hurricane Floyd.  Minor water damage toward the bottom of the pages.  All text is still legible.  At least two pages at the end appear to have been torn out given the paper chunks in the back of the staple, but it is likely this occurred prior to the flooding due to a lack of smeared ink on the back of the current final page (6).
+	
 	The document is presented By Eileen Ramsay in its original form starting on page 2.
 </pre>
 
@@ -13,16 +15,16 @@
   <img src="../../assets/images/scc09/page-2.jpg" alt="SCC-09-02" width="50%">
 </figure>
 <figure>
-  <img src="../../assets/images/scc09/page-3.jpg" alt="SCC-09-03">
+  <img src="../../assets/images/scc09/page-3.jpg" alt="SCC-09-03" width="50%">
 </figure>
 <figure>
-  <img src="../../assets/images/scc09/page-4.jpg" alt="SCC-09-04">
+  <img src="../../assets/images/scc09/page-4.jpg" alt="SCC-09-04" width="50%">
 </figure>
 <figure>
-  <img src="../../assets/images/scc09/page-5.jpg" alt="SCC-09-05">
+  <img src="../../assets/images/scc09/page-5.jpg" alt="SCC-09-05" width="50%">
 </figure>
 <figure>
-  <img src="../../assets/images/scc09/page-6.jpg" alt="SCC-09-06">
+  <img src="../../assets/images/scc09/page-6.jpg" alt="SCC-09-06" width="50%">
 </figure>
 
 </div>
