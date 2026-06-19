b36dc427e57a19af6dc88114c65df0b88a405d04
Zachary Michniewicz <ztmich@gmail.com>
2026-06-19 18:44:53 -0400

    bios

--- wedding/wedding-party.php
+++ wedding/wedding-party.php
@@ -13,7 +13,8 @@
 
     <h2 data-en="Best Man" data-es="Padrino de Boda">Best Man</h2>
 	<img src="..\assets\images\wedding\coming_soon.png" alt="Kristen" width="400" height="500">
-    <p data-en="Kristen &mdash; Placeholder bio goes here." data-es="Kristen &mdash; La biografía de marcador de posición va aquí.">Kristen &mdash; Placeholder bio goes here.</p>
+    <h3 data-en="Kristen" data-es="Kristen">Kristen</h3>
+	<p data-en="Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without." data-es="">Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without.</p>
 
     <h2 data-en="Bridesmaids" data-es="Damas de Honor">Bridesmaids</h2>
 	<img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500">
@@ -28,14 +29,14 @@
 
     <h2 data-en="Groomsmen" data-es="Padrinos">Groomsmen</h2>
     <img src="..\assets\images\wedding\coming_soon.png" alt="Adam" width="400" height="500">
-	<p data-en="Adam &mdash; Placeholder bio goes here." data-es="Adam &mdash; La biografía de marcador de posición va aquí.">Adam &mdash; Placeholder bio goes here.
-    </p>
+	<h3 data-en="Adam" data-es="Adam">Adam</h3>
+	<p data-en="Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life." data-es="">Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life.</p>
 	<img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500">
-	<p data-en="Kevin &mdash; Placeholder bio goes here." data-es="Kevin &mdash; La biografía de marcador de posición va aquí.">Kevin &mdash; Placeholder bio goes here.
-    </p>
+	<h3 data-en="Kevin" data-es="Kevin">Kevin</h3>
+	<p data-en="" data-es=""></p>
 	<img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500">
-	<p data-en="Keegan &mdash; Placeholder bio goes here." data-es="Keegan &mdash; La biografía de marcador de posición va aquí.">Keegan &mdash; Placeholder bio goes here.
-    </p>
+	<h3 data-en="Keegan" data-es="Keegan">Keegan</h3>
+	<p data-en="Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out." data-es="">Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out.</p>
 </section>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/wedding/includes/footer.php"); ?>
