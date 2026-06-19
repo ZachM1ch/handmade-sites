605cdeb2e47ecf40238dede6614fc745cc83124e
Zachary Michniewicz <ztmich@gmail.com>
2026-06-19 17:53:19 -0400

    Pictures!

--- wedding/wedding-party.php
+++ wedding/wedding-party.php
@@ -8,21 +8,33 @@
     </p>
 
     <h2 data-en="Maid of Honor" data-es="Dama de Honor">Maid of Honor</h2>
-    <p data-en="Name &mdash; Placeholder bio goes here." data-es="Nombre &mdash; La biografía de marcador de posición va aquí.">Name &mdash; Placeholder bio goes here.</p>
+	<img src="..\assets\images\wedding\cian.png" alt="Cian" width="400" height="500">
+    <p data-en="Cian &mdash; Placeholder bio goes here." data-es="Cian &mdash; La biografía de marcador de posición va aquí.">Cian &mdash; Placeholder bio goes here.</p>
 
     <h2 data-en="Best Man" data-es="Padrino de Boda">Best Man</h2>
-    <p data-en="Name &mdash; Placeholder bio goes here." data-es="Nombre &mdash; La biografía de marcador de posición va aquí.">Name &mdash; Placeholder bio goes here.</p>
+	<img src="..\assets\images\wedding\coming_soon.png" alt="Kristen" width="400" height="500">
+    <p data-en="Kristen &mdash; Placeholder bio goes here." data-es="Kristen &mdash; La biografía de marcador de posición va aquí.">Kristen &mdash; Placeholder bio goes here.</p>
 
     <h2 data-en="Bridesmaids" data-es="Damas de Honor">Bridesmaids</h2>
-    <p data-en="Placeholder: Add bridesmaid names and bios here."
-       data-es="Marcador de posición: Agregue los nombres y biografías de las damas de honor aquí.">
-        Placeholder: Add bridesmaid names and bios here.
+	<img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500">
+	<p data-en="Monica &mdash; Placeholder bio goes here." data-es="Monica &mdash; La biografía de marcador de posición va aquí.">Monica &mdash; Placeholder bio goes here.
+    </p>
+	<img src="..\assets\images\wedding\jessica.png" alt="Jessica" width="400" height="500">
+	<p data-en="Jessica &mdash; Placeholder bio goes here." data-es="Jessica &mdash; La biografía de marcador de posición va aquí.">Jessica &mdash; Placeholder bio goes here.
+    </p>
+	<img src="..\assets\images\wedding\jennifer.png" alt="Jennifer" width="400" height="500">
+	<p data-en="Jennifer &mdash; Placeholder bio goes here." data-es="Jennifer &mdash; La biografía de marcador de posición va aquí.">Jennifer &mdash; Placeholder bio goes here.
     </p>
 
     <h2 data-en="Groomsmen" data-es="Padrinos">Groomsmen</h2>
-    <p data-en="Placeholder: Add groomsmen names and bios here."
-       data-es="Marcador de posición: Agregue los nombres y biografías de los padrinos aquí.">
-        Placeholder: Add groomsmen names and bios here.
+    <img src="..\assets\images\wedding\coming_soon.png" alt="Adam" width="400" height="500">
+	<p data-en="Adam &mdash; Placeholder bio goes here." data-es="Adam &mdash; La biografía de marcador de posición va aquí.">Adam &mdash; Placeholder bio goes here.
+    </p>
+	<img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500">
+	<p data-en="Kevin &mdash; Placeholder bio goes here." data-es="Kevin &mdash; La biografía de marcador de posición va aquí.">Kevin &mdash; Placeholder bio goes here.
+    </p>
+	<img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500">
+	<p data-en="Keegan &mdash; Placeholder bio goes here." data-es="Keegan &mdash; La biografía de marcador de posición va aquí.">Keegan &mdash; Placeholder bio goes here.
     </p>
 </section>
 
