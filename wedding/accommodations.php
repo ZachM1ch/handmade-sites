92a7f8ba4b684b8cf2cd773e84404601de6044da
Zachary Michniewicz <ztmich@gmail.com>
2026-06-21 18:55:23 -0400

    m dash

--- wedding/accommodations.php
+++ wedding/accommodations.php
@@ -8,9 +8,9 @@
     </p>
 
     <h2 data-en="Courtyard by Mariott" data-es="Courtyard by Mariott">Courtyard by Mariott</h2>
-    <p data-en="300 Corporate Dr, Lebanon, NJ 08833.  Phone: (908) 236-8500"
-       data-es="300 Corporate Dr, Lebanon, NJ 08833.  Teléfono: (908) 236-8500">
-        300 Corporate Dr, Lebanon, NJ 08833.  Phone: (908) 236-8500
+    <p data-en="300 Corporate Dr, Lebanon, NJ 08833  &mdash;  Phone: (908) 236-8500"
+       data-es="300 Corporate Dr, Lebanon, NJ 08833  &mdash;  Teléfono: (908) 236-8500">
+        300 Corporate Dr, Lebanon, NJ 08833  &mdash;  Phone: (908) 236-8500
     </p>
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3027.2633592611564!2d-74.8335171!3d40.6461247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c38c4dbba4cfeb%3A0x7ffe3a7340085937!2sCourtyard%20by%20Marriott%20Lebanon!5e0!3m2!1sen!2sus!4v1782082364761!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
