5fd8cd5801d2f3890a8fd882cc434450d554816a
Zachary Michniewicz <ztmich@gmail.com>
2026-06-21 16:58:21 -0400

    AHHHHHHHHHHHHHHHHH^2

--- wedding/wedding-party.php
+++ wedding/wedding-party.php
@@ -12,14 +12,14 @@ include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/header.php"); ?>
     <div class="center-flex">
         <div class="center-box-flex">
             <h2 style="text-align:center" data-en="Maid of Honor" data-es="Dama de Honor">Maid of Honor</h2>
-            <img src="..\assets\images\wedding\cian.png" alt="Cian" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\cian.png" alt="Cian" width="400" height="500"></div>
             <h3 data-en="Cian" data-es="Cian">Cian</h3>
             <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
                 bio goes here.</p>
         </div>
         <div class="center-box-flex">
             <h2 style="text-align:center" data-en="Best Man" data-es="Padrino de Boda">Best Man</h2>
-            <img src="..\assets\images\wedding\coming_soon.png" alt="Kristen" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\coming_soon.png" alt="Kristen" width="400" height="500"></div>
             <h3 data-en="Kristen" data-es="Kristen">Kristen</h3>
             <p data-en="Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without."
                data-es="">Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class. Through
@@ -37,19 +37,19 @@ include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/header.php"); ?>
     <div><h2 style="text-align:center data-en=" Bridesmaids" data-es="Damas de Honor">Bridesmaids</h2></div>
     <div class="center-flex">
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500"></div>
             <h3 data-en="Monica" data-es="Monica">Monica</h3>
             <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
                 bio goes here.</p>
         </div>
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\jessica.png" alt="Jessica" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\jessica.png" alt="Jessica" width="400" height="500"></div>
             <h3 data-en="Jessica" data-es="Jessica">Jessica</h3>
             <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
                 bio goes here.</p>
         </div>
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\jennifer.png" alt="Jennifer" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\jennifer.png" alt="Jennifer" width="400" height="500"></div>
             <h3 data-en="Jennifer" data-es="Jennifer">Jennifer</h3>
             <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
                 bio goes here.</p>
@@ -61,7 +61,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/header.php"); ?>
     <div><h2 data-en="Groomsmen" data-es="Padrinos">Groomsmen</h2></div>
     <div class="center-flex">
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\coming_soon.png" alt="Adam" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\coming_soon.png" alt="Adam" width="400" height="500"></div>
             <h3 data-en="Adam" data-es="Adam">Adam</h3>
             <p data-en="Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life."
                data-es="">Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon
@@ -78,12 +78,12 @@ include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/header.php"); ?>
                 my life.</p>
         </div>
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500"></div>
             <h3 data-en="Kevin" data-es="Kevin">Kevin</h3>
             <p data-en="" data-es=""></p>
         </div>
         <div class="center-box-flex">
-            <img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500">
+            <div class="center-image"><img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500"></div>
             <h3 data-en="Keegan" data-es="Keegan">Keegan</h3>
             <p data-en="Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out."
                data-es="">Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born. Once
