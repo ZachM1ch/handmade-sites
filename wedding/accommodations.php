6ff403a7e3474f78210ba52eb9d90e48bb799f7e
Zachary Michniewicz <ztmich@gmail.com>
2026-06-21 18:51:30 -0400

    accomodation updates

--- wedding/accommodations.php
+++ wedding/accommodations.php
@@ -12,6 +12,13 @@
        data-es="300 Corporate Dr, Lebanon, NJ 08833.  Teléfono: (908) 236-8500">
         300 Corporate Dr, Lebanon, NJ 08833.  Phone: (908) 236-8500
     </p>
+
+    <iframe src="https://www.google.com/maps/place/Courtyard+by+Marriott+Lebanon/@40.6461247,-74.8335171,17z/data=!3m1!4b1!4m9!3m8!1s0x89c38c4dbba4cfeb:0x7ffe3a7340085937!5m2!4m1!1i2!8m2!3d40.6461247!4d-74.8335171!16s%2Fm%2F0wq66lk?entry=ttu&g_ep=EgoyMDI2MDYxNi4wIKXMDSoASAFQAw%3D%3D" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <p>
+        <a class="btn" href="https://maps.google.com/?q=300+Corporate+Dr+Lebanon+NJ" target="_blank"
+           data-en="Open in Google Maps" data-es="Abrir en Google Maps">Open in Google Maps</a>
+    </p>
 </section>
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/wedding/includes/footer.php"); ?>
