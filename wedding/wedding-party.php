ad8894b0665047d4a20ece79698942c409f58787
Zachary Michniewicz <ztmich@gmail.com>
2026-06-21 16:43:34 -0400

    Adjusted div styling crazy style

--- wedding/wedding-party.php
+++ wedding/wedding-party.php
@@ -1,51 +1,89 @@
-<?php $pageTitle = "Wedding Party"; include($_SERVER['DOCUMENT_ROOT']."/wedding/includes/header.php"); ?>
+<?php $pageTitle = "Wedding Party";
+include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/header.php"); ?>
 
 <section class="card">
-	<div class="center-flex">
-		<div>
-			<h1 style="text-align:center" data-en="Wedding Party" data-es="Cortejo Nupcial">Wedding Party</h1>
-    		<p style="text-align:center" data-en="We are honored to celebrate with these wonderful people by our sides."
-       		data-es="Es un honor celebrar con estas maravillosas personas a nuestro lado.">
-     		We are honored to celebrate with these wonderful people by our sides.
-    		</p>
-		</div>
-	</div>
-	<div class="center-flex">
+    <div>
+        <h1 style="text-align:center" data-en="Wedding Party" data-es="Cortejo Nupcial">Wedding Party</h1>
+        <p style="text-align:center" data-en="We are honored to celebrate with these wonderful people by our sides."
+           data-es="Es un honor celebrar con estas maravillosas personas a nuestro lado.">
+            We are honored to celebrate with these wonderful people by our sides.
+        </p>
+    </div>
+    <div class="center-flex">
         <div class="center-box-flex">
             <h2 style="text-align:center" data-en="Maid of Honor" data-es="Dama de Honor">Maid of Honor</h2>
             <img src="..\assets\images\wedding\cian.png" alt="Cian" width="400" height="500">
             <h3 data-en="Cian" data-es="Cian">Cian</h3>
-            <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder bio goes here.</p>
+            <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
+                bio goes here.</p>
         </div>
         <div class="center-box-flex">
             <h2 style="text-align:center" data-en="Best Man" data-es="Padrino de Boda">Best Man</h2>
             <img src="..\assets\images\wedding\coming_soon.png" alt="Kristen" width="400" height="500">
             <h3 data-en="Kristen" data-es="Kristen">Kristen</h3>
-            <p data-en="Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without." data-es="">Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without.</p>
+            <p data-en="Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class.  Through high school we went to a bunch of concerts together and stayed close even after she moved away for college.  During COVID we spent a ton of time together playing games and watching movies together and keeping each other sane.  Even when our communications take a lull when life gets complex, every time we come back together it’s just like no time had passed at all.  Now Alina and I always take an annual trip in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!  She is truly someone I could not imagine living without."
+               data-es="">Kristen and I met back in high school in Ms Nicol’s Intro to Computer Science class. Through
+                high school we went to a bunch of concerts together and stayed close even after she moved away for
+                college. During COVID we spent a ton of time together playing games and watching movies together and
+                keeping each other sane. Even when our communications take a lull when life gets complex, every time we
+                come back together it’s just like no time had passed at all. Now Alina and I always take an annual trip
+                in the beginning of fall to visit them, but in 2027 looks like they’ll be coming up to see us for this!
+                She is truly someone I could not imagine living without.</p>
         </div>
-	</div>
-    
-    <h2 data-en="Bridesmaids" data-es="Damas de Honor">Bridesmaids</h2>
-	<img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500">
-	<p data-en="Monica &mdash; Placeholder bio goes here." data-es="Monica &mdash; La biografía de marcador de posición va aquí.">Monica &mdash; Placeholder bio goes here.
-    </p>
-	<img src="..\assets\images\wedding\jessica.png" alt="Jessica" width="400" height="500">
-	<p data-en="Jessica &mdash; Placeholder bio goes here." data-es="Jessica &mdash; La biografía de marcador de posición va aquí.">Jessica &mdash; Placeholder bio goes here.
-    </p>
-	<img src="..\assets\images\wedding\jennifer.png" alt="Jennifer" width="400" height="500">
-	<p data-en="Jennifer &mdash; Placeholder bio goes here." data-es="Jennifer &mdash; La biografía de marcador de posición va aquí.">Jennifer &mdash; Placeholder bio goes here.
-    </p>
+    </div>
 
+    <div class="center-flex">
+        <h2 style="text-align:center data-en="Bridesmaids" data-es="Damas de Honor">Bridesmaids</h2>
+        <div class="center-box-flex">
+            <img src="..\assets\images\wedding\monica.png" alt="Monica" width="400" height="500">
+            <h3 data-en="Monica" data-es="Monica">Monica</h3>
+            <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
+                bio goes here.</p>
+        </div>
+        <div class="center-box-flex">
+            <img src="..\assets\images\wedding\jessica.png" alt="Jessica" width="400" height="500">
+            <h3 data-en="Jessica" data-es="Jessica">Jessica</h3>
+            <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
+                bio goes here.</p>
+        </div>
+        <div class="center-box-flex">
+            <img src="..\assets\images\wedding\jennifer.png" alt="Jennifer" width="400" height="500">
+            <h3 data-en="Jennifer" data-es="Jennifer">Jennifer</h3>
+            <p data-en="Placeholder bio goes here." data-es="La biografía de marcador de posición va aquí.">Placeholder
+                bio goes here.</p>
+        </div>
+    </div>
+
+    <div class="center-flex">
     <h2 data-en="Groomsmen" data-es="Padrinos">Groomsmen</h2>
+        <div class="center-box-flex">
     <img src="..\assets\images\wedding\coming_soon.png" alt="Adam" width="400" height="500">
-	<h3 data-en="Adam" data-es="Adam">Adam</h3>
-	<p data-en="Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life." data-es="">Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life.</p>
-	<img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500">
-	<h3 data-en="Kevin" data-es="Kevin">Kevin</h3>
-	<p data-en="" data-es=""></p>
-	<img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500">
-	<h3 data-en="Keegan" data-es="Keegan">Keegan</h3>
-	<p data-en="Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out." data-es="">Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out.</p>
+    <h3 data-en="Adam" data-es="Adam">Adam</h3>
+    <p data-en="Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon kindergarten class.  Without question he is one of my longest and closest friends.  Over 22 years of friendship we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my spiral into owning too many board games and giving us an excuse to play more of them.  I’ve gone to the same school as Adam functionally my entire life, through elementary school, middle school, high school, community college, and university and now we work together, our desks only 30 feet apart.  He’s always been a constant in my life."
+       data-es="">Adam and I have know each other since we were 5 years old way back in Ms Viggiano’s afternoon
+        kindergarten class. Without question he is one of my longest and closest friends. Over 22 years of friendship
+        we’ve had countless sleepovers and late night game sessions, and he always been so eager to indulge Alina and my
+        spiral into owning too many board games and giving us an excuse to play more of them. I’ve gone to the same
+        school as Adam functionally my entire life, through elementary school, middle school, high school, community
+        college, and university and now we work together, our desks only 30 feet apart. He’s always been a constant in
+        my life.</p>
+        </div>
+        <div class="center-box-flex">
+    <img src="..\assets\images\wedding\coming_soon.png" alt="Kevin" width="400" height="500">
+    <h3 data-en="Kevin" data-es="Kevin">Kevin</h3>
+    <p data-en="" data-es=""></p>
+        </div>
+        <div class="center-box-flex">
+    <img src="..\assets\images\wedding\coming_soon.png" alt="Keegan" width="400" height="500">
+    <h3 data-en="Keegan" data-es="Keegan">Keegan</h3>
+    <p data-en="Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born.  Once he moved to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds and go swimming.  Some of my most foundational memories playing games, hanging out, and going to the beach are with Keegan.  I cannot remember what he looked like before he grew his hair out."
+       data-es="">Of the three members of my groomsmen, as my cousin, I’ve known Keegan since he was born. Once he moved
+        to the other side of town from my childhood home, he, my sister Emily, and I spent nearly every day together as
+        kids, especially over summer where we would go to Myya and Grandpa’s house every Wednesday, grab some McDonalds
+        and go swimming. Some of my most foundational memories playing games, hanging out, and going to the beach are
+        with Keegan. I cannot remember what he looked like before he grew his hair out.</p>
+        </div>
+    </div>
 </section>
 
-<?php include($_SERVER['DOCUMENT_ROOT']."/wedding/includes/footer.php"); ?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/wedding/includes/footer.php"); ?>
